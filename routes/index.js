5999c24326233b73edf0ba3040e17d23388f337f
Ebubekir Tabak <ebubekir.tabak@yahoo.com>
2019-09-28 09:24:06 +0300

    Started crawler when page load.

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const router = express.Router();
 const { HttpService } = require('../services/http');
+const { Crawler } = require('../services/crawler');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   const url = 'https://www.chevron.com';
   const httpService = new HttpService();
   httpService.getHtmlDocumentByUrl(url);
+  new Crawler().startCrawler(url);
   res.render('index', { title: 'NodeCrawler' });
 });
 
