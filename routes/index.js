802b0b356750cd300fa2bcbc3dad49e04f32dae7
Ebubekir Tabak <ebubekir.tabak@yahoo.com>
2019-09-30 00:18:32 +0300

    index route has been connect to query params.

--- routes/index.js
+++ routes/index.js
@@ -6,11 +6,18 @@ const { Crawler } = require('../services/crawler');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  const url = 'https://www.chevron.com';
-  const options = { keyword: 'Announces' };
-  new Crawler(url, options).startCrawler(url).then((resolve) => {
-      res.render('index', { title: 'NodeCrawler', result: resolve });
-  });
+    req.setTimeout(3600000);
+    const { query } = req;
+    if (query && query.url && query.keyword) {
+        const url = query.url;
+        const options = {keyword: query.keyword};
+        new Crawler(url, options).startCrawler(url).then((resolve) => {
+            const json = JSON.parse(resolve);
+            res.render('index', {title: 'NodeCrawler', result: json});
+        });
+    } else {
+        res.render('index', {title: 'NodeCrawler', error: 'parameters not found.'});
+    }
 });
 
 module.exports = router;
