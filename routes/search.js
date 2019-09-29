050da7fc857ebc138c500144330c505bbe672819
Ebubekir Tabak <ebubekir.tabak@yahoo.com>
2019-09-29 20:28:59 +0300

    Search route added.

---
+++ routes/search.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const router = express.Router();
+const event = require("events");
+const { HttpService } = require('../services/http');
+const { Crawler } = require('../services/crawler');
+
+/* GET Search page. */
+router.get('/', function(req, res, next) {
+    const { query } = req;
+    if (query && query.url && query.keyword) {
+        const url = query.url;
+        const options = { keyword: query.keyword };
+        new Crawler(url, options).startCrawler(url).then((resolve) => {
+            const json = JSON.parse(resolve);
+            res.json(json);
+        });
+    } else {
+        res.json({});
+    }
+});
+
+module.exports = router;
